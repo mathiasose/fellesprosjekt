367ced84b5273ecb61df3d8b3a39cc9e1bcb958b
Mathias Ose <mathias.ose@gmail.com>
2014-03-04 10:58:59 +0100

    update db.sql

--- db/db.sql
+++ db/db.sql
@@ -1,44 +1,45 @@
-CREATE TABLE Person (
+CREATE TABLE Employee (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(50),
     phone VARCHAR(16),
     epost VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(128) NOT NULL,
+    invitationToAppointment INT,
     PRIMARY KEY(id);
+    CONSTRAINT FOREIGN KEY (invitationToAppointment) REFERENCES (Appointment(id)), 
 );
 
-CREATE TABLE Employee (
-    constraint Employee_fk references Person(id),
-    password VARCHAR(20) NOT NULL -- security hole anyone?
+CREATE TABLE Group (
+    id INT NOT NULL,
+    member INT NOT NULL,
+    subgroup INT,
+    PRIMARY KEY(id);
+    CONSTRAINT FOREIGN KEY (member) REFERENCES (Employee(id)),
+    CONSTRAINT FOREIGN KEY (subgroup) REFERENCES (Group(id)),
 );
 
-CREATE TABLE Non-employee (
-    constraint Non-employee_fk references Person(id)
-);
-
-CREATE TABLE GGroup (
-    id INT NOT NULL AUTO_INCREMENT
-);
-
-CREATE TABlE Appointment (
+CREATE TABLE Appointment (
     id INT NOT NULL AUTO_INCREMENT,
-    start_time DATETIME2 NOT NULL,
+    start_time DATETIME NOT NULL,
     duration TIME NOT NULL,
     location VARCHAR(20),
-    -- status?
-);
-
-CREATE TABLE Calendar (
-    constraint Calendar_Employee references ?,
-    constraint Calendar_Appointment references Appointment(id)
+    reservation INT,
+    canceled BOOLEAN,
+    PRIMARY KEY (id)
+    CONSTRAINT FOREIGN KEY (reservation) REFERENCES (Room(id)), 
 );
 
 CREATE TABLE Notification (
-
+    employee_id INT NOT NULL,
+    appointment_id INT NOT NULL,
+    PRIMARY KEY (employee_id, appointment_id)
+    CONSTRAINT FOREIGN KEY (employee_id) REFERENCES (Employee(id)),  
+    CONSTRAINT FOREIGN KEY (appointment_id) REFERENCES (Appointment(id)),
 );
 
 CREATE TABLE Room (
     id INT NOT NULL AUTO_INCREMENT,
-    capaticy INT
+    capacity INT,
+    PRIMARY KEY (id)
 );
-
 
