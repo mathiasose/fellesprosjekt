f39d8ad055d9a093113300474217816c09891d6d
Mathias Ose <mathias.ose@gmail.com>
2014-03-05 13:09:05 +0100

    Java can talk to DB

--- java_app/src/db/DBConnection.java
+++ java_app/src/db/DBConnection.java
@@ -1,13 +1,12 @@
 package db;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 
 public class DBConnection {
 
     public static Connection connect() {
         try {
-            String url = "jdbc:mysql://127.0.0.1/db/db.sql";
+            String url = "jdbc:mysql://127.0.0.1/test";
             String username = "root";
             String password = "root";
             return DriverManager.getConnection(url, username, password);
@@ -15,4 +14,18 @@ public class DBConnection {
             throw new RuntimeException("Unable to connect", e);
         }
     }
+    
+    public static void main(String[] args) {
+		Connection connection = connect();
+		
+		try {
+			Statement stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from Employee");
+			while(rs.next()) {
+				System.out.println(rs.getString("name"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
