1fe94097a3bdf2634427d6278149897c8d5a387f
Mathias Ose <mathias.ose@gmail.com>
2014-03-05 11:40:16 +0100

    db connection

--- db/db.sql
+++ db/db.sql
@@ -2,20 +2,20 @@ CREATE TABLE Employee (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(50),
     phone VARCHAR(16),
-    epost VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(128) NOT NULL,
     invitationToAppointment INT,
-    PRIMARY KEY(id);
-    CONSTRAINT FOREIGN KEY (invitationToAppointment) REFERENCES (Appointment(id)), 
+    PRIMARY KEY(id),
+    CONSTRAINT FOREIGN KEY (invitationToAppointment) REFERENCES Appointment(id)
 );
 
 CREATE TABLE Group (
     id INT NOT NULL,
     member INT NOT NULL,
     subgroup INT,
-    PRIMARY KEY(id);
-    CONSTRAINT FOREIGN KEY (member) REFERENCES (Employee(id)),
-    CONSTRAINT FOREIGN KEY (subgroup) REFERENCES (Group(id)),
+    PRIMARY KEY(id),
+    CONSTRAINT FOREIGN KEY (member) REFERENCES Employee(id),
+    CONSTRAINT FOREIGN KEY (subgroup) REFERENCES Group(id)
 );
 
 CREATE TABLE Appointment (
@@ -25,16 +25,16 @@ CREATE TABLE Appointment (
     location VARCHAR(20),
     reservation INT,
     canceled BOOLEAN,
-    PRIMARY KEY (id)
-    CONSTRAINT FOREIGN KEY (reservation) REFERENCES (Room(id)), 
+    PRIMARY KEY (id),
+    CONSTRAINT FOREIGN KEY (reservation) REFERENCES Room(id)
 );
 
 CREATE TABLE Notification (
     employee_id INT NOT NULL,
     appointment_id INT NOT NULL,
-    PRIMARY KEY (employee_id, appointment_id)
-    CONSTRAINT FOREIGN KEY (employee_id) REFERENCES (Employee(id)),  
-    CONSTRAINT FOREIGN KEY (appointment_id) REFERENCES (Appointment(id)),
+    PRIMARY KEY (employee_id, appointment_id),
+    CONSTRAINT FOREIGN KEY (employee_id) REFERENCES Employee(id),
+    CONSTRAINT FOREIGN KEY (appointment_id) REFERENCES Appointment(id)
 );
 
 CREATE TABLE Room (
@@ -43,3 +43,5 @@ CREATE TABLE Room (
     PRIMARY KEY (id)
 );
 
+INSERT INTO Employee (name, email, password) VALUES
+  ("Mathias Ose", "m@thiaso.se", "hunter2");
