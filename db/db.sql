420e24c74365db47ebcd7fe38abb599e43d25fa4
Mathias Ose <mathias.ose@gmail.com>
2014-03-11 10:36:14 +0100

    update db.sql

--- db/db.sql
+++ db/db.sql
@@ -27,21 +27,34 @@ CREATE TABLE Employee (
     CONSTRAINT FOREIGN KEY (invitationToAppointment) REFERENCES Appointment(id)
 );
 
-CREATE TABLE ggroup (
+CREATE TABLE EmployeeGroup (
     id INT NOT NULL,
-    member INT NOT NULL,
-    subgroup INT,
     PRIMARY KEY(id),
-    CONSTRAINT FOREIGN KEY (member) REFERENCES Employee(id),
-    CONSTRAINT FOREIGN KEY (subgroup) REFERENCES ggroup(id)
+);
+
+CREATE TABLE Subgroup (
+   child_id INT NOT NULL,
+   parent_id INT NOT NULL,
+   CONSTRAINT FOREIGN KEY (child_id) REFERENCES EmployeeGroup(id),
+   CONSTRAINT FOREIGN KEY (parent_id) REFERENCES EmployeeGroup(id),
+);
+
+CREATE TABLE Invitation (
+    employee_id INT NOT NULL,
+    appointment_id INT NOT NULL,
+    notification_id INT,
+    attending BOOLEAN,
+    creator BOOLEAN NOT NULL,
+    hidden BOOLEAN NOT NULL,
+    PRIMARY KEY (employee_id, appointment_id),
+    CONSTRAINT FOREIGN KEY (employee_id) REFERENCES Employee(id),
+    CONSTRAINT FOREIGN KEY (appointment_id) REFERENCES Appointment(id),
+    CONSTRAINT FOREIGN KEY (notification_id) REFERENCES Notification(id)
 );
 
 CREATE TABLE Notification (
-    employee_id INT NOT NULL,
-    appointment_id INT NOT NULL,
-    PRIMARY KEY (employee_id, appointment_id),
-    CONSTRAINT FOREIGN KEY (employee_id) REFERENCES Employee(id),
-    CONSTRAINT FOREIGN KEY (appointment_id) REFERENCES Appointment(id)
+    id INT NOT NULL AUTO_INCREMENT,
+    time DATETIME NOT NULL
 );
 
 INSERT INTO Employee (name, email, password) VALUES
