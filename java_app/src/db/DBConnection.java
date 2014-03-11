3fafc41f6965ff0b1a64d0013575439ccf2cd796
Freboto <toernvall@gmail.com>
2014-03-11 12:13:26 +0100

    la til noen metoder i DBConnect

--- java_app/src/db/DBConnection.java
+++ java_app/src/db/DBConnection.java
@@ -6,54 +6,62 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.ResultSet;
 
-
 public class DBConnection {
-	
+
 	private java.sql.Connection connection = null;
 
-    public static Connection connect() {
-        try {
-            String url = "jdbc:mysql://mysql.stud.ntnu.no/tornvall_felles";
-            String username = "tornvall_g3";
-            String password = "123abc";
-            return DriverManager.getConnection(url, username, password);
-        } catch (Exception e) {
-            throw new RuntimeException("Unable to connect", e);
-        }
-    }
-    
-    public void setConnection(java.sql.Connection connection){
-    	this.connection = connection;
-    }
-    
-    public java.sql.Connection getConnection(){
-    	
-    	return this.connection;
-    	
-    }
-    
-    /////////////////lager metode for å legge til appointment i DB//////////////////////
-    
-   /* public void setAppointment(){
-    	try {
-    		Statement stmt = connection.createStatement();
-    		ResultSet rs = stmt.executeQuery("insert into Appointment(location) values('HAHAHAHAHHA')");
-    	}
-    	catch (SQLException e) {
-    		e.printStackTrace();
-    	}
-    }*/
-    
-    ///////////////////////////////////////////////////////////////////////////////////
-    
-    public static void main(String[] args) {
-		Connection connection = connect();
-		
+	public static Connection connect() {
 		try {
-			Statement stmt = connection.createStatement();
-			stmt.executeUpdate("insert into Appointment(start_time, duration, location) values('2014-03-07 09:10:10', '01:01:01', 'HAHAHAHAHHA')");
-			ResultSet rs = stmt.executeQuery("select * from Employee");
-			while(rs.next()) {
+			String url = "jdbc:mysql://mysql.stud.ntnu.no/tornvall_felles";
+			String username = "tornvall_g3";
+			String password = "123abc";
+			return DriverManager.getConnection(url, username, password);
+		} catch (Exception e) {
+			throw new RuntimeException("Unable to connect", e);
+		}
+	}
+
+	public void setConnection(java.sql.Connection connection) {
+		this.connection = connection;
+	}
+
+	public java.sql.Connection getConnection() {
+		return this.connection;
+	}
+
+	public static void getAppointment() {
+
+	}
+
+	// ///////////////lager metode for å legge til appointment i
+	// DB//////////////////////
+
+	public void addAppointment() {
+		try {
+			query("insert into Appointment(location) values('HAHAHAHAHHA')");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// /////////////////////////////////////////////////////////////////////////////////
+
+	public static void update(String update) throws SQLException {
+		Statement stmt = connect().createStatement();
+		stmt.executeUpdate(update);
+	}
+	
+	public static ResultSet query(String query) throws SQLException {
+		Statement stmt = connect().createStatement();
+		return stmt.executeQuery(query);
+	}
+
+	public static void main(String[] args) {
+		try {
+			update("insert into Appointment(start_time, duration, location) values('2014-5-17 12:00:00', '01:01:01', 'spise is')");
+			
+			ResultSet rs = query("select * from Employee");
+			while (rs.next()) {
 				System.out.println(rs.getString("name"));
 			}
 		} catch (SQLException e) {
