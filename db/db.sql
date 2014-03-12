8443c784e0be145dd7d40e0ab930cff761769428
Mathias Ose <mathias.ose@gmail.com>
2014-03-12 11:28:22 +0100

    fix keys

--- db/db.sql
+++ db/db.sql
@@ -17,8 +17,9 @@ CREATE TABLE Appointment (
 CREATE TABLE Reservation (
     appointment_id INT NOT NULL,
     room_id INT NOT NULL,
-    CONSTRAINT FOREIGN KEY (appointment_id) REFERENCES Appointment(id),
-    CONSTRAINT FOREIGN KEY (room_id) REFERENCES Room(id)
+    PRIMARY KEY (appointment_id, room_id),
+    CONSTRAINT reservation_fk_appointment FOREIGN KEY (appointment_id) REFERENCES Appointment(id),
+    CONSTRAINT reservation_fk_room FOREIGN KEY (room_id) REFERENCES Room(id)
 );
 
 CREATE TABLE Employee (
@@ -38,15 +39,17 @@ CREATE TABLE EmployeeGroup (
 CREATE TABLE Subgroup (
    child_id INT NOT NULL,
    parent_id INT NOT NULL,
-   CONSTRAINT FOREIGN KEY (child_id) REFERENCES EmployeeGroup(id),
-   CONSTRAINT FOREIGN KEY (parent_id) REFERENCES EmployeeGroup(id)
+   PRIMARY KEY (child_id, parent_id),
+   CONSTRAINT subgroup_fk_child FOREIGN KEY (child_id) REFERENCES EmployeeGroup(id),
+   CONSTRAINT subgroup_fk_parent FOREIGN KEY (parent_id) REFERENCES EmployeeGroup(id)
 );
 
 CREATE TABLE Member (
     employee_id INT NOT NULL,
     group_id INT NOT NULL,
-    CONSTRAINT FOREIGN KEY (employee_id) REFERENCES Employee(id),
-    CONSTRAINT FOREIGN KEY (group_id) REFERENCES EmployeeGroup(id)
+    PRIMARY KEY (employee_id, group_id),
+    CONSTRAINT member_fk_employee FOREIGN KEY (employee_id) REFERENCES Employee(id),
+    CONSTRAINT member_fk_group FOREIGN KEY (group_id) REFERENCES EmployeeGroup(id)
 );
 
 CREATE TABLE Notification (
@@ -62,9 +65,10 @@ CREATE TABLE Invitation (
     attending BOOLEAN,
     creator BOOLEAN NOT NULL,
     hidden BOOLEAN NOT NULL,
-    CONSTRAINT FOREIGN KEY (employee_id) REFERENCES Employee(id),
-    CONSTRAINT FOREIGN KEY (appointment_id) REFERENCES Appointment(id),
-    CONSTRAINT FOREIGN KEY (notification_id) REFERENCES Notification(id)
+    PRIMARY KEY (employee_id, appointment_id),
+    CONSTRAINT invitation_fk_employee FOREIGN KEY (employee_id) REFERENCES Employee(id),
+    CONSTRAINT invitation_fk_appointment FOREIGN KEY (appointment_id) REFERENCES Appointment(id),
+    CONSTRAINT invitation_fk_notification FOREIGN KEY (notification_id) REFERENCES Notification(id)
 );
 
 INSERT INTO Employee (name, email, password) VALUES
