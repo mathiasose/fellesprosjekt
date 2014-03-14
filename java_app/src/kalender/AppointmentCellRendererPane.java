21980e20854578f98777c941cc11c2d00a8103c6
Krisspear <kristrd@stud.ntnu.no>
2014-03-14 11:28:46 +0100

    "Mathias sin kule renderer"

---
+++ java_app/src/kalender/AppointmentCellRendererPane.java
@@ -0,0 +1,12 @@
+package kalender;
+
+import javax.swing.CellRendererPane;
+import javax.swing.JButton;
+
+public class AppointmentCellRendererPane extends CellRendererPane {
+	public AppointmentCellRendererPane() {
+		super();
+		this.add(new JButton());
+	}
+	
+}
